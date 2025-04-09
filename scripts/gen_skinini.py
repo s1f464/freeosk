34535f0443a91f13971b905acc1b2d5435ac4db2
s1f464 <>
2025-04-09 06:47:03 +0000

    Refactor gen_skinini.py

--- scripts/gen_skinini.py
+++ scripts/gen_skinini.py
@@ -5,39 +5,40 @@ import configparser
 import subprocess
 import sys
 
-GIT_PATH = "git"
-SKIN_INI_INPUT = "src/skin.ini"
-SKIN_INI_OUTPUT = "build/skin.ini"
+DEFAULT_AUTHOR = "Unknown"
+DEFAULT_GIT = "git"
+DEFAULT_INPUT = "src/skin.ini"
+DEFAULT_OUTPUT = "build/skin.ini"
 
 
 def main():
     args = parse_args()
 
-    commit_hash = get_latest_commit_hash()
-    commit_date = get_latest_commit_date()
+    commit_hash = get_latest_commit_hash(args.git)
+    commit_date = get_latest_commit_date(args.git)
 
     config = configparser.ConfigParser(comment_prefixes="//", delimiters=[":"])
     config.optionxform = lambda option: option
 
-    with open(SKIN_INI_INPUT, "r") as f:
+    with open(args.input, "r") as f:
         config.read_file(f)
         config["General"]["Name"] += f" ({commit_hash} {commit_date})"
         config["General"]["Author"] = args.author
 
-    with open(SKIN_INI_OUTPUT, "w") as f:
+    with open(args.output, "w") as f:
         config.write(f)
 
 
-def get_latest_commit_hash() -> str:
-    cmd = [GIT_PATH, "rev-parse", "--short", "HEAD"]
+def get_latest_commit_hash(git: str) -> str:
+    cmd = [git, "rev-parse", "--short", "HEAD"]
     proc = subprocess.run(cmd, capture_output=True, text=True)
     if proc.returncode != 0:
         return "unknown"
     return str(proc.stdout.strip())
 
 
-def get_latest_commit_date() -> str:
-    cmd = [GIT_PATH, "log", "-1", "--format=%ad", "--date", "format:%Y-%m-%d"]
+def get_latest_commit_date(git: str) -> str:
+    cmd = [git, "log", "-1", "--format=%ad", "--date", "format:%Y-%m-%d"]
     proc = subprocess.run(cmd, capture_output=True, text=True)
     if proc.returncode != 0:
         return "unknown"
@@ -45,10 +46,24 @@ def get_latest_commit_date() -> str:
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
     _ = parser.add_argument(
-        "--author", default="Unknown", metavar="NAME", help="set author"
+        "--author", default=DEFAULT_AUTHOR, help="set author", metavar="NAME"
+    )
+
+    _ = parser.add_argument(
+        "--git", default=DEFAULT_GIT, help="path to git", metavar="PATH"
+    )
+
+    _ = parser.add_argument(
+        "-i", "--input", default=DEFAULT_INPUT, help="path to input", metavar="PATH"
+    )
+
+    _ = parser.add_argument(
+        "-o", "--output", default=DEFAULT_OUTPUT, help="path to output", metavar="PATH"
     )
 
     return parser.parse_args()
